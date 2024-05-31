75a9da423de084d2b44761e0f09d58212c51e66b
clean2001 <clearrworld@gmail.com>
2024-05-31 17:34:16 +0900

    Feat: 음원에 좋아요 누르기 기능 추가

---
+++ procedure-sejeong/음원에좋아요누르기.sql
@@ -0,0 +1,15 @@
+DELIMITER //
+CREATE PROCEDURE `음원에좋아요누르기`(IN 음원아이디 BIGINT, IN 회원아이디 BIGINT)
+BEGIN
+    DECLARE 데이터개수 INT;
+    SET 데이터개수 = 0;
+
+    SELECT COUNT(*) INTO 데이터개수 FROM music_like WHERE music_id = 음원아이디 AND member_id = 회원아이디;
+
+    IF 데이터개수 > 0 THEN
+       DELETE FROM music_like WHERE music_id = 음원아이디 AND member_id = 회원아이디;
+    ELSE
+        INSERT INTO music_like(music_id , member_id) VALUES (음원아이디, 회원아이디);
+    END IF;
+END
+// DELIMITER ;
