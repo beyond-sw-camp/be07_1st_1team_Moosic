bd1f05ecc3eaf679c0ec1e65de5af6aeb455cc07
clean2001 <clearrworld@gmail.com>
2024-05-31 22:50:53 +0900

    Feat: 통합검색 프로시저 추가

--- sejeong-procedure/sejeong-all-procedures.sql
+++ sejeong-procedure/sejeong-all-procedures.sql
@@ -139,3 +139,16 @@ BEGIN
     GROUP BY music.music_title ;
 END
 // DELIMITER ;
+
+
+-- 12. 통합 검색
+DELIMITER //
+CREATE PROCEDURE `통합검색`(IN 검색어 VARCHAR(253))
+BEGIN
+    SELECT '음원' AS type, music_id AS id, music_title AS `title or name` FROM music WHERE music.music_title LIKE CONCAT(CONCAT('%', 검색어), '%')
+    UNION ALL
+    SELECT '앨범' AS type, album_id AS id, album_title AS `title or name` FROM album WHERE album.album_title LIKE CONCAT(CONCAT('%', 검색어), '%')
+    UNION ALL
+    SELECT '아티스트' AS type, artist_id AS id, artist_name AS `title or name` FROM artist WHERE artist.artist_name LIKE CONCAT(CONCAT('%', 검색어), '%');
+END
+// DELIMITER ;
