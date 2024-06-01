4d5bdd14d4e8e99f9abdaa3d5602ab5d0e0c4e98
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-06-01 17:04:00 +0900

    Create 음악생성프로시.sql
    music 테이블 데이터 INSERT 및 
    album 테이블의 music_count 값 1 증가

---
+++ Procedure/음악생성프로시.sql
@@ -0,0 +1,34 @@
+DELIMITER //
+
+CREATE PROCEDURE make_music
+	(IN in_title varchar(255)
+    , IN in_lyrics varchar(10000)
+    , IN in_img varchar(2083)
+    , IN in_pt int(10)
+    , IN in_id BIGINT
+    , IN in_genre enum('k-pop','pop','j-pop','dance','ballad','hip-hop','trot'))
+    
+BEGIN
+	INSERT INTO `moosic`.`music`
+(`music_title`,
+`lyrics`,
+`music_file_url`,
+`playtime`,
+`album_id`,
+`genre`)
+VALUES
+(in_title,
+in_lyrics,
+in_img,
+in_pt,
+in_id,
+in_genre);
+
+UPDATE album 
+SET music_count = music_count + 1
+WHERE album_id = in_id;
+END //
+
+DELIMITER ;
+
+CALL make_music("HEO_TORR_SONG", "wal wal wal wal", "example.com", 300, 61, 'hip-hop');
