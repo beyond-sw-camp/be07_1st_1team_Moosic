c90105672021a1ae6fd7fbd9de271e0ed3926d86
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-06-03 10:02:00 +0900

    Update 로그아웃기능.sql

--- procedure/member/로그아웃기능.sql
+++ procedure/member/로그아웃기능.sql
@@ -7,14 +7,10 @@ CREATE PROCEDURE 로그아웃(
     in input varchar(30)
 )
 BEGIN
-    -- login_check 라는 int 타입의 변수 선언
     declare member_check int;
     
-    -- 입력받은 이메일과 비밀번호가 member 의 이메일과 비밀번호와 같다면 login_check 에 1을 넣는다.
     select 1 into member_check from member where member_email = m_member_email and password = m_password;
 	
-    -- login_check 가 1이라면 로그인 성공 출력
-    -- 1이 아니라면 잘못된 로그인 정보입니다. 를 출력
     if member_check = 1 then
 		select '로그아웃 성공' as 결과창 where input = '로그아웃';
 	end if;
